d8089ed8564e43cdb5f745f0816ff87c2f566cd1
thuan <thuanngo30072002@gmail.com>
2023-10-11 08:52:40 +0700

    8/10: handle save song

--- src/main/java/com/spotify/app/security/auth/RegisterRequest.java
+++ src/main/java/com/spotify/app/security/auth/RegisterRequest.java
@@ -1,18 +1,26 @@
 package com.spotify.app.security.auth;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class RegisterRequest {
-    private String firstName ;
-    private String lastName ;
-    private String email;
-    private String password;
+public record RegisterRequest(@NotBlank String firstName,
+                              @NotBlank String lastName,
+                              @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$")
+                              @NotBlank String email,
+
+                              @Size(min = 8, message = "the password must be at least 8 characters")
+                              String password,
+                              int day,
+                              @NotBlank
+                              int month,
+                              int year,
+                              @NotBlank
+                              String gender
+) {
 
 }

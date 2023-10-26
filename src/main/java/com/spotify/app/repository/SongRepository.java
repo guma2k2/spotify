a6f2bb954d1ddda42b68f67ed8885f0d0afb0447
thuan <thuanngo30072002@gmail.com>
2023-10-26 21:12:53 +0700

    26/10:update search song by name

--- src/main/java/com/spotify/app/repository/SongRepository.java
+++ src/main/java/com/spotify/app/repository/SongRepository.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Repository
 public interface SongRepository extends JpaRepository<Song, Long> {
--- src/test/java/com/spotify/app/TestConfig.java
+++ src/test/java/com/spotify/app/TestConfig.java
@@ -1,7 +1,5 @@
 package com.spotify.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;

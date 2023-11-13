04a21a42f8e80ae83f847d96a7d254df913fdf7f
thuan <thuanngo30072002@gmail.com>
2023-11-13 22:19:19 +0700

    13/11: testing

--- src/test/java/com/spotify/app/AbstractTestcontainers.java
+++ src/test/java/com/spotify/app/AbstractTestcontainers.java
@@ -1,59 +1,59 @@
-package com.spotify.app;
-
-import lombok.extern.slf4j.Slf4j;
-import org.flywaydb.core.Flyway;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.MountableFile;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-
-@Testcontainers
-@Slf4j
-public abstract class AbstractTestcontainers {
-
-
-    @BeforeAll
-    static void beforeAll() {
-        Flyway flyway = Flyway
-                .configure()
-                .dataSource(
-                        mySQLContainer.getJdbcUrl(),
-                        mySQLContainer.getUsername(),
-                        mySQLContainer.getPassword()
-                ).load();
-        flyway.migrate();
-    }
-    @Container
-    protected static final MySQLContainer<?> mySQLContainer =
-            new MySQLContainer<>("mysql:latest")
-                    .withDatabaseName("spotify-dao-unit-test")
-                    .withUsername("kai")
-                    .withPassword("password")
-            ;
-
-    @DynamicPropertySource
-    static void dynamicProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", mySQLContainer::getUsername);
-        registry.add("spring.datasource.password", mySQLContainer::getPassword);
-    }
-
+//package com.spotify.app;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.flywaydb.core.Flyway;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.test.context.DynamicPropertyRegistry;
+//import org.springframework.test.context.DynamicPropertySource;
+//import org.testcontainers.containers.MySQLContainer;
+//import org.testcontainers.junit.jupiter.Container;
+//import org.testcontainers.junit.jupiter.Testcontainers;
+//import org.testcontainers.utility.MountableFile;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//
+//
+//@Testcontainers
+//@Slf4j
+//public abstract class AbstractTestcontainers {
+//
+//
 //    @BeforeAll
-//    static void setUp() {
-//        mySQLContainer.withInitScript("schema.sql");
-//        mySQLContainer.start();
+//    static void beforeAll() {
+//        Flyway flyway = Flyway
+//                .configure()
+//                .dataSource(
+//                        mySQLContainer.getJdbcUrl(),
+//                        mySQLContainer.getUsername(),
+//                        mySQLContainer.getPassword()
+//                ).load();
+//        flyway.migrate();
 //    }
-
-    @Test
-    void connectionEstablished() {
-        assertThat(mySQLContainer.isCreated()).isTrue();
-        assertThat(mySQLContainer.isRunning()).isTrue();
-    }
-}
+//    @Container
+//    protected static final MySQLContainer<?> mySQLContainer =
+//            new MySQLContainer<>("mysql:latest")
+//                    .withDatabaseName("spotify-dao-unit-test")
+//                    .withUsername("kai")
+//                    .withPassword("password")
+//            ;
+//
+//    @DynamicPropertySource
+//    static void dynamicProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+//        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+//        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+//    }
+//
+////    @BeforeAll
+////    static void setUp() {
+////        mySQLContainer.withInitScript("schema.sql");
+////        mySQLContainer.start();
+////    }
+//
+//    @Test
+//    void connectionEstablished() {
+//        assertThat(mySQLContainer.isCreated()).isTrue();
+//        assertThat(mySQLContainer.isRunning()).isTrue();
+//    }
+//}
--- src/test/java/com/spotify/app/journey/UserIT.java
+++ src/test/java/com/spotify/app/journey/UserIT.java
@@ -1,53 +1,53 @@
-package com.spotify.app.journey;
-
-import com.spotify.app.AbstractTestcontainers;
-import com.spotify.app.dto.UserDTO;
-import com.spotify.app.dto.response.UserResponse;
-import com.spotify.app.security.jwt.JwtService;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Slf4j
-public class UserIT  {
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Autowired
-    private JwtService jwtService;
-
-    private static final String USER_PATH = "/api/v1/user";
-
-    @Test
-    public void shouldReturnUser__WhenGivenValidId() {
-        // given
-        Long userId = 1L;
-        String url = USER_PATH.concat(String.format("/%d",userId));
-//        log.info(url);
-        ResponseEntity<UserDTO> response = restTemplate.
-                exchange(url, HttpMethod.GET,null,UserDTO.class);
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isNotNull();
-    }
-
+//package com.spotify.app.journey;
+//
+//import com.spotify.app.AbstractTestcontainers;
+//import com.spotify.app.dto.UserDTO;
+//import com.spotify.app.dto.response.UserResponse;
+//import com.spotify.app.security.jwt.JwtService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@Slf4j
+//public class UserIT  {
+//
+//    @Autowired
+//    private TestRestTemplate restTemplate;
+//
+//    @Autowired
+//    private JwtService jwtService;
+//
+//    private static final String USER_PATH = "/api/v1/user";
+//
 //    @Test
-//    public void shouldReturnFail__WhenGivenUnValidId(){
-//        Long userId = 0L ;
+//    public void shouldReturnUser__WhenGivenValidId() {
+//        // given
+//        Long userId = 1L;
 //        String url = USER_PATH.concat(String.format("/%d",userId));
 ////        log.info(url);
 //        ResponseEntity<UserDTO> response = restTemplate.
-//                getForEntity(url,UserDTO.class);
-////        log.info(String.valueOf(response.getBody()));
-//        log.info(String.valueOf(response.getStatusCode()));
+//                exchange(url, HttpMethod.GET,null,UserDTO.class);
+//        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        assertThat(response.getBody()).isNotNull();
 //    }
-
-}
+//
+////    @Test
+////    public void shouldReturnFail__WhenGivenUnValidId(){
+////        Long userId = 0L ;
+////        String url = USER_PATH.concat(String.format("/%d",userId));
+//////        log.info(url);
+////        ResponseEntity<UserDTO> response = restTemplate.
+////                getForEntity(url,UserDTO.class);
+//////        log.info(String.valueOf(response.getBody()));
+////        log.info(String.valueOf(response.getStatusCode()));
+////    }
+//
+//}
